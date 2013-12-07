095a3a10245cdf3427835b612a6887bedd42c2a2
Jie Liu <jie.liu@rea-group.com>
2013-12-07 14:18:26 +0800

    sdf

---
+++ bsd.sh
@@ -0,0 +1,7 @@
+mkdir -p src/main/java/
+mkdir -p src/main/webapp/WEB-INF/pages
+mkdir -p src/test/java/
+cp /Users/twer/Library/springframework/HelloWeb-servlet.xml src/main/webapp/WEB-INF/
+cp /Users/twer/Library/springframework/web.xml src/main/webapp/WEB-INF/
+cp /Users/twer/Library/springframework/build.gradle .
+
